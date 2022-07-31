198b0b744ebb89478c0f7c21088cdd0359e84a73
Harold Cheng <niuchangcun@gmail.com>
2022-07-31 21:50:19 +0800

    cache lab part A

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -11,6 +11,17 @@
 
 #define address_bits 64
 
+typedef struct cache_line {
+    char valid;  // valid bit is the lowest 1 bit
+
+    // since addresses are 64 bits, tag bits won't be greater than 64 bits
+    int64_t tag;
+
+    // We just need to print a hit/miss result
+    // So we don't need to actually store the data
+    // char* block;
+} cache_line;
+
 typedef cache_line* data_t;
 typedef struct linklist_node {
     struct linklist_node* prev;
@@ -23,38 +34,32 @@ typedef struct linklist {
     linklist_node* tail;
 } linklist;
 
-void init_linklist(linklist* l);
-void print_help_message(char* program_name);
-
-typedef struct cache_line {
-    char valid;  // valid bit is the lowest 1 bit
-
-    // since addresses are 64 bits, tag bits won't be greater than 64 bits
-    int64_t tag;
-
-    // We just need to print a hit/miss result
-    // So we don't need to actually store the data
-    // char* block;
-} cache_line;
 typedef struct cache {
     linklist* sets;  // an array of lisklists
     int line_num;
     int set_index_bits;
     int associativity;
     int block_bits;
+    int tag_bits;
+    int set_num;
 } cache;
 
-int set_index_bits = 0, associativity = 0, block_bits = 0, verbose = 0;
-int tag_bits = 0;
-int set_num = 0;
+int verbose = 0;
 int hit_cnt = 0, miss_cnt = 0, eviction_cnt = 0;
 cache c;
 
+void init_linklist(linklist* l);
+void print_help_message(char* program_name);
 void init_cache();
+cache_line* read_cache(linklist* set, int64_t tag);
+cache_line* search_lines(linklist* set, int64_t tag);
+void load_address(linklist* set, int64_t tag);
 
 int main(int argc, char** argv) {
     int option;
     char* tracefile = NULL;
+
+    c = (cache){NULL, 0, 0, 0, 0, 0, 0};
 
     while ((option = getopt(argc, argv, "s:E:b:t:hv")) != -1) {
         switch (option) {
@@ -64,13 +69,13 @@ int main(int argc, char** argv) {
             case 's':
                 // optarg is a pointer points to somewhere in argv
                 // so it's safe to reassign this variable
-                set_index_bits = atoi(optarg);
+                c.set_index_bits = atoi(optarg);
                 break;
             case 'E':
-                associativity = atoi(optarg);
+                c.associativity = atoi(optarg);
                 break;
             case 'b':
-                block_bits = atoi(optarg);
+                c.block_bits = atoi(optarg);
                 break;
             case 't':
                 tracefile = optarg;
@@ -84,20 +89,63 @@ int main(int argc, char** argv) {
         }
     }
 
-    if (!tracefile || !associativity || !set_index_bits || !block_bits) {
+    if (!tracefile || !c.associativity || !c.set_index_bits || !c.block_bits) {
         printf("%s: Missing required command line argument\n", argv[0]);
         return -1;
     }
 
-    printf("%d %d %d %s\n", set_index_bits, associativity, block_bits,
-           tracefile);
+    c.tag_bits = address_bits - (c.block_bits + c.set_index_bits);
+    assert(c.tag_bits > 0);
+    c.set_num = (int)pow(2, c.set_index_bits);
+    // printf(
+    //     "set_index_bits: %d\n"
+    //     "set_num: %d\n"
+    //     "associativity: %d\n"
+    //     "block_bits: %d\n"
+    //     "tag_bits: %d\n",
+    //     c.set_index_bits, c.set_num, c.associativity, c.block_bits,
+    //     c.tag_bits);
+    init_cache();
 
-    tag_bits = address_bits - (block_bits + set_index_bits);
-    assert(tag_bits > 0);
-    set_num = (int)pow(2, set_index_bits);
-    cache* c = init_cache();
+    FILE* fp;
+    int ch;
+    char buf[30];
+    char action;
+    int64_t address;
+    int size;
+    fp = fopen(tracefile, "r");
+    // read 1 char, skip if not a space
+    while ((ch = getc(fp)) != EOF) {
+        fgets(buf, 30, fp);
+        if (ch != ' ') {
+            continue;
+        }
+        sscanf(buf, "%c %lx,%d", &action, &address, &size);
+        int64_t set_index =
+            (address >> c.block_bits) & ((1 << c.set_index_bits) - 1);
+        int64_t tag = (address >> c.block_bits) >> c.set_index_bits;
+        // printf("set_index: %ld, tag: %ld\n", set_index, tag);
+        linklist* set = &(c.sets[set_index]);
+        printf("%c %lx,%d", action, address, size);
+        switch (action) {
+            case 'L':
+                read_cache(set, tag);
+                break;
+            case 'S':
+                read_cache(set, tag);
+                break;
+            case 'M':
+                read_cache(set, tag);
+                read_cache(set, tag);
+                break;
 
-    printSummary(0, 0, 0);
+            default:
+                abort();
+        }
+        printf("\n");
+    }
+
+    printSummary(hit_cnt, miss_cnt, eviction_cnt);
     return 0;
 }
 
@@ -126,12 +174,12 @@ void init_linklist(linklist* l) {
 }
 
 void init_cache() {
-    c.sets = malloc(sizeof(linklist) * set_num);
-    for (int i = 0; i < set_num; i++) {
+    c.sets = malloc(sizeof(linklist) * c.set_num);
+    for (int i = 0; i < c.set_num; i++) {
         linklist* set = &(c.sets[i]);
         init_linklist(set);
 
-        for (int j = 0; j < associativity; j++) {
+        for (int j = 0; j < c.associativity; j++) {
             linklist_node* node = malloc(sizeof(linklist_node));
 
             // insert a new node to the beginning of the linklist
@@ -147,17 +195,49 @@ void init_cache() {
     }
 }
 
-cache_line* read_cache(int64_t address, int size) {
-    int64_t set_index = (address >> c.block_bits) & (1 << c.set_index_bits - 1);
+cache_line* read_cache(linklist* set, int64_t tag) {
+    if (search_lines(set, tag) == NULL) {
+        load_address(set, tag);
+    }
+    return NULL;
 }
 
-void search_lines(linklist* set) {
+cache_line* search_lines(linklist* set, int64_t tag) {
     linklist_node* cur = set->head->next;
-    int miss = 0, hit = 0, eviction = 0;
+    // int miss = 0, hit = 0, eviction = 0;
     while (cur != set->tail) {
         cache_line* line = cur->data;
         // it is not possible that a valid line is located after an invalid line
         if (!line->valid) {
-            miss = 1;
+            if (verbose) printf(" miss");
+            miss_cnt++;
+            return NULL;
         }
+        if (line->tag == tag) {
+            if (verbose) printf(" hit");
+            hit_cnt++;
+            return line;
+        }
+        cur = cur->next;
     }
+    if (verbose) printf(" miss eviction");
+    miss_cnt++;
+    eviction_cnt++;
+    return NULL;
+}
+
+void load_address(linklist* set, int64_t tag) {
+    // get the LRU line
+    linklist_node* node = set->tail->prev;
+    cache_line* line = node->data;
+    if (!line->valid) line->valid = 1;
+    line->tag = tag;
+
+    // move node to the head
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    node->prev = set->head;
+    node->next = set->head->next;
+    set->head->next->prev = node;
+    set->head->next = node;
+}
